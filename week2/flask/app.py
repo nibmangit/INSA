034f1315ebabf56855ad88d919a50d30a2de5f51
Nibretu Mengaw <nibretumengaw@gmail.com>
2026-08-07 12:06:06 +0300

    create the database configuration and sample moder and understand the syntax and the concept how it works

--- week2/flask/app.py
+++ week2/flask/app.py
@@ -1,6 +1,25 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__) # Create a Flask app instance
+
+#Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # Configure the database URI for SQLAlchemy
+db = SQLAlchemy(app) # Create a SQLAlchemy database instance
+
+#models
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return '<Task %r>' % self.id
+    
+
+
+
 
 @app.route('/') # Define a route for the root URL
 def hello():
