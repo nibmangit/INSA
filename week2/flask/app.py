3b18d73708e2ad6bc31b3d1a54836e6d25fc982d
Nibretu Mengaw <nibretumengaw@gmail.com>
2026-08-07 13:26:17 +0300

    add the post method and make sure the post works and the data save in the test.db and render the html table with the conotent of the db

--- week2/flask/app.py
+++ week2/flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -21,10 +21,21 @@ class Todo(db.Model):
 
 
 
-@app.route('/') # Define a route for the root URL
+@app.route('/', methods=['GET', 'POST']) # Define a route for the root URL
 def hello():
-    return render_template('index.html') # Render the 'index.html' template when the root URL is accessed
-    # return 'Hello, World!' # Return a simple response
+    if request.method ==  'POST':
+        task_content = request.form["task"]
+        new_task = Todo(content=task_content)
+        
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding the task."
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks = tasks)  
 
 if __name__ == '__main__': # Check if the script is run directly
     app.run(debug=True) # Run the app in debug mode
