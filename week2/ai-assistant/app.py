977903b415b65194194f6118a671f7651162535b
Nibretu Mengaw <nibretumengaw@gmail.com>
2026-08-14 11:55:20 +0300

    add the post chat api endpoint and test the endpoint in postman if it correctly accept the request and return response

---
+++ week2/ai-assistant/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/api/chat', methods=["POST"])
+def chat():
+    data = request.get_json()
+    message = data["message"]
+    print("User Message: ", message)
+    
+    return jsonify({"response":"The message is recived seccessfully."})
+
+if __name__ == "__main__":
+    app.run(debug=True)
