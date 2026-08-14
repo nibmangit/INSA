9870b3a547ced36890cb200f95ffa66b6fc12ae5
Nibretu Mengaw <nibretumengaw@gmail.com>
2026-08-14 13:40:58 +0300

    Add the Gemini client and send the user message to gemini and replay the response to the user

--- week2/ai-assistant/app.py
+++ week2/ai-assistant/app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template, request, jsonify
+from google import genai
+
+with open(".env", "r") as file:
+    line = file.readline()
+key, value = line.strip().split("=", 1)
+
+client = genai.Client(api_key = value)
 
 app = Flask(__name__)
 
@@ -13,9 +20,12 @@ def chat():
     if not message:
         return jsonify({"error":"Message is required"}), 400
     
-    print("User Message: ", message)
+    response = client.models.generate_content(
+        model="models/gemini-3.6-flash",
+        contents=message
+    ) 
     
-    return jsonify({"response":"The message is recived seccessfully."})
+    return jsonify({"response": response.text})
 
 if __name__ == "__main__":
     app.run(debug=True)
