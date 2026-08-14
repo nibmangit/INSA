a420d0cefeed75ff62bed705621d9aeea733e971
Nibretu Mengaw <nibretumengaw@gmail.com>
2026-08-14 14:48:37 +0300

    accept the AI response and display the user message and ai message in teh chat box accordingly

--- week2/ai-assistant/static/script.js
+++ week2/ai-assistant/static/script.js
@@ -1,8 +1,31 @@
 const form = document.getElementById("chat-form");
 const input = document.getElementById("user-input");
+const chatBox = document.getElementById("chat-box");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    sendMessage();
+});
+
+function addMessage(message, type) {
+    const messageElement = document.createElement("div");
+    messageElement.classList.add("message", type);
+
+    const content = document.createElement("div");
+    content.classList.add("message-content");
+    content.textContent = message;
+
+    messageElement.appendChild(content);
+    chatBox.appendChild(messageElement);
+}
+
 
 async function sendMessage() {
     const message = input.value;
+
+    addMessage(message, "user-message");
+    input.value = "";
 
     const response = await fetch("/api/chat", {
         method: "POST",
@@ -16,11 +39,5 @@ async function sendMessage() {
 
     const data = await response.json();
 
-    console.log("Server response:", data);
+    addMessage(data.response, "ai-message"); 
 }
-
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    sendMessage();
-});
